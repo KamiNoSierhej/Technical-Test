e53434c4291b3bcd6e02acb9c2233c90cf65aee2
KamiNoSierhej <sierhej10@gmail.com>
2018-12-15 19:09:22 +0100

    Heroku deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Technical-Test.wsgi
--- interviewapp/settings.py
+++ interviewapp/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -179,3 +181,23 @@ SITE_ID = 1
 DATA_SOURCE_FOR_CREDIT_INFORMATION = (
     'https://functionapp20180527095701.azurewebsites.net/api/CreditCheckCall'
 )
+
+# heroku
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
